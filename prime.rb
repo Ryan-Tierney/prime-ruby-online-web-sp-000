1a861aef1d6cc0516bb65c9b27940f18faa32b0e
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-29 08:38:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,30 @@
-def prime?(numbers)
-    if numbers%2 !=0 && numbers!=2
-      true 
-    else 
-      false 
-    end
+def sorting(top)
+
+  prime_numbers = (0..top).to_a
+
+
+  prime_numbers[0] = prime_numbers[1] = nil
+
+  # Iterate through primes array
+  counter = 0
+  prime_numbers.each do |i|
+    # Skip if nil
+    next unless i
+
+    # Break if we are past the square root of the max value 
+    break if i*i > top
+    counter += 1
+    # Start at the square of the current number, and step through.
+    # Go up to the max value, by multiples of the current number, and replace
+    # that value with nil in the primes array
+    (i*i).step(top,i) { |y| prime_numbers[y] = nil }
   end
+
+  # Finally, return the compacted array.
+  puts "Solved for #{top} in #{counter} steps."
+  prime_numbers.compact
+end
+
+def prime?(number)
+  sorting(number).include?(number)
+end
